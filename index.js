845f9bd20c01c3de360347b6c79990e27978504e
SirJimKe <jimmymutuku1@gmail.com>
2022-08-26 11:12:18 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  const reverseStr = reversingAString(word);
+  if(word === reverseStr){
+    return true
+  } else {
+    return false
+  }
 }
+
+function reversingAString(word){
+  let newString = '';
+  for(let i = word.length - 1; i >= 0; i--){
+    newString += word[i]
+  }
+  return newString;
+}
+
 
 /* 
   Add your pseudocode here
 */
-
+//reverse a sting
+//compare if the original string is equal to the reversed string
+// if equal return true, else return false
 /*
   Add written explanation of your solution here
 */
 
-// You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
@@ -20,6 +35,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("wow"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("code"));
 }
 
 module.exports = isPalindrome;
